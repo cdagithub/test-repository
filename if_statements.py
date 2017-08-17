a2b24c86241276f7664ef25257ce1c99e6476350
CDA <gd_carmen@yahoo.com>
2017-08-17 11:29:17 +0100

    Modified file if_statements.py

--- if_statements.py
+++ if_statements.py
@@ -1,10 +1,46 @@
-people = ["John", "Anna", "Mary"]
+#known_people = ["John", "Anna", "Mary"]
 
-person = input("Enter a name: ")
+#person = input("Enter the person you know: ")
 
-if person in people:
-    print("{} is in my list!".format(person))
-else:
-    print("{} is NOT in my list!".format(person))
+#if person in known_people:
+#    print("You know {}!".format(person))
+#else:
+#    print("You don't know {}!".format(person))    
 
-print("Done")
+
+def who_do_you_know():
+    people_string = input("Enter the names of people you know, separated by commas: ")
+
+    people_list = []
+    person_name = ""
+    for i in people_string:
+        if i == ",":
+            if person_name != "":
+                people_list.append(person_name)
+                person_name = ""        
+        else:
+            if i != " ":
+                person_name = person_name + i
+    if person_name != "":
+        people_list.append(person_name)
+    return people_list
+   
+
+def ask_user(person_list):
+    name = input("Enter a name: ")
+    if name in person_list:
+        print("You know {}!".format(name))
+    else:
+        print("You don't know {}!".format(name))    
+
+user_list = who_do_you_know()
+for person in user_list:
+    print(person)
+
+should_continue = "y"
+while should_continue == "y":
+    should_continue = input("Do you want to continue? (y/n) ")
+    if should_continue == "y":
+        ask_user(user_list)
+
+print ("Done")    
